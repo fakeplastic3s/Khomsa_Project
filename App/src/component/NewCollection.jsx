d03e4ac2034d79dfae48b43d287ad7aa25beb383
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-13 18:26:03 +0700

    update

--- App/src/component/NewCollection.jsx
+++ App/src/component/NewCollection.jsx
@@ -3,7 +3,7 @@ import { Card } from "flowbite-react";
 
 function NewCollection() {
   return (
-    <div className="w-full bg-slate-50 py-6 px-4">
+    <div className="w-full bg-slate-50 py-24 px-4">
       <h1 className="font-bold pb-5 text-xl uppercase text-center">New Collection</h1>
       <div className="max-w-[1240px] mx-auto flex flex-wrap gap-10 justify-center items-center ">
         <Card className="max-w-sm" imgSrc="/produk/1.JPG" horizontal>
