af2f9eb239c4ed2e8f0805863bc71c2933529fc1
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-13 13:48:21 +0700

    navbar

---
+++ App/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./component/navbar";
+import Hero from "./component/Hero";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ App/src/component/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="max-w-[800px] mt[-96px] w-full h-screen text-center mx-auto flex flex-col justify-center">
+        <p className="text-[#607274] font-bold p-2">Welcome to Khomsa_</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Reach the pinnacle of fashion with us!</h1>
+        <div>
+          <p>Trendsetting Styles, Quality Craftsmanship, Inclusive Fashion for </p>
+        </div>
+        <h1></h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
