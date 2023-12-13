a3d092a7a69702c974e1f6e61ea061178faab91b
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-13 15:51:56 +0700

    add hero and content

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./component/navbar";
 import Hero from "./component/Hero";
+import NewCollection from "./component/NewCollection";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
       <Hero />
+      <NewCollection />
     </>
   );
 }
